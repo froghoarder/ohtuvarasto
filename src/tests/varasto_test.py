800254f4374f79e013c86c5832750940a0b0d4a7
Nea <neazahorszky@gmail.com>
2024-11-04 23:37:44 +0200

    Fix tests

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -37,7 +37,7 @@ class TestVarasto(unittest.TestCase):
         self.varasto.lisaa_varastoon(8)
 
         # vapaata tilaa pitäisi vielä olla tilavuus-lisättävä määrä eli 2
-        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 0)
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 2)
 
     def test_lisäys_ei_mahdu(self):
         varasto = Varasto(0)
